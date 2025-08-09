75d79260daf123c2a2e084cfc4d14509c98604ce
zer0stat <jessemartincurtis@gmail.com>
2025-08-09 10:45:57 -0700

    patch 1.2

--- firefox extensions/pagecount to wordcount/substitution.js
+++ firefox extensions/pagecount to wordcount/substitution.js
@@ -1,8 +1,20 @@
 // number of words in a "page"
 const CONVERSION = 275;
 
+/**
+ * This script activates on the search page of royalroad.com and grabs the html element
+ * with the different books data. Then it loops through each one finding the text
+ * that displays the page count. It converts it to a word count and rearranges itself
+ * into a pretty format with comma seperation. Job done, grab a beer.
+ */
+
 // grabs list of fics
-const fics = document.getElementById("result").children;
+let fics = document.getElementById("result");
+if (fics != null) {
+    fics = fics.children;
+} else {
+    fics = document.querySelector(".fiction-list").children;
+}
 
 // grabs each fic in list
 for (let i = 0; i < fics.length; i++) {
@@ -12,8 +24,9 @@ for (let i = 0; i < fics.length; i++) {
     pages_data = pages_data.querySelector("span");
 
     // processes the page count into an ordered array of digits
-    temp_data = pages_data.innerHTML.split(" ");
-    temp_data = Number(temp_data[0]) * CONVERSION
+    temp_data = pages_data.textContent.split(" ");
+    temp_data = temp_data[0].replaceAll(",","");
+    temp_data = Number(temp_data) * CONVERSION
     temp_data = temp_data.toString().split("");
 
     // processes array into a number divided by commas every third digit
@@ -27,5 +40,5 @@ for (let i = 0; i < fics.length; i++) {
     temp_data = pretty_number + " words";
     
     // replaces the page count with new word count
-    pages_data.innerHTML = temp_data;
+    pages_data.textContent = temp_data;
 }
